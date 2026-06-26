db5e43602f4fa5eb3624ccad4306b4728c363c98
fortinux <info@fortinux.com>
2026-06-26 11:37:40 +0200

    Primer commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/servicios')
+def servicios():
+    return render_template('servicios.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('base.html')
